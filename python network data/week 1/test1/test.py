cd545a2b80acd61d25426354b48a5024e953106d
skiddling <skiddling@gmail.com>
2015-11-09 22:08:52 +0800

    finish the first task

---
+++ python network data/week 1/test1/test.py
@@ -0,0 +1,6 @@
+sum = 0
+
+for number in range(1, 5):
+    sum += number
+
+print "The sum is", sum
---
+++ python network data/week 1/test2/test.py
@@ -0,0 +1,17 @@
+import re
+
+# file = open("regex_sum_42.txt")
+# file = open("regex_sum_197949.txt")
+
+# vlist = []
+# for line in file:
+# vlist += re.findall('[0-9]+', line)
+
+# sum = 0
+# for e in vlist:
+# sum += int(e)
+# print sum
+
+line = 'from fwjiefja@wfaefa wfa waefaaw wfa'
+y = re.findall('@([^ ]*)', line)
+print y
